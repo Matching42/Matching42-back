43ce4416a3a55dce3d4be1f1d5ed4412485217a7
seo2im <seo2im6492@gmail.com>
2021-05-16 16:03:23 +0900

    feature:controller example 제작 - /example로 날릴 때, json을 반환하는 router 예시 등록

--- srcs/app.tsx
+++ srcs/app.tsx
@@ -2,8 +2,21 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 
-import { db_connect } from './config';
+import { connectDB } from './config';
+import router from './routes';
 
-db_connect(process.env.DB_URL ? process.env.DB_URL : '');
-const app = express();
-app.listen(process.env.PORT, () => console.log(`server port ${process.env.PORT}`));
+const runServer = async () => {
+    console.log('Chcek DB connection...');
+    await connectDB(process.env.DB_URL ? process.env.DB_URL : '');
+
+    console.log('Set application...');
+    const app = express();
+
+    /* Set middleware */
+    app.use(router);
+
+    /* Run server */
+    app.listen(process.env.PORT, () => console.log(`server Run with port: ${process.env.PORT}`));
+};
+
+runServer();
