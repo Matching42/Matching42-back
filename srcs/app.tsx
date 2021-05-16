81b790dfc2dc47b5fbdeef08c4363d4ed538399a
seo2im <seo2im6492@gmail.com>
2021-05-16 15:41:14 +0900

    feature: DB 연결 module 작성 - mongoose활용 atlas상의 Matching42Dummy DB로 연결

--- srcs/app.tsx
+++ srcs/app.tsx
@@ -2,5 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 
+import { db_connect } from './config';
+
+db_connect(process.env.DB_URL ? process.env.DB_URL : '');
 const app = express();
 app.listen(process.env.PORT, () => console.log(`server port ${process.env.PORT}`));
