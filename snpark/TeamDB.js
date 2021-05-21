d80d5686149d26a3809801f33a595db15273467c
snagjaepark <atimangel@naver.com>
2021-05-21 10:12:41 +0900

    feature: teamDB schema, enum 수정 및 model export -schema 내부 ;으로 구분하던 것 ,로 교체 -TeamState enum 내부 인자 문자열로 만듬 -state 속성 Number에서 String으로 바꿈 -model export -collection name 지정? -시작 날짜랑 종료 날짜 속성은 없어도 되는 건가요?

--- snpark/TeamDB.js
+++ snpark/TeamDB.js
@@ -1,10 +1,12 @@
 import mongoose from 'mongoose';
 
+mongoose.set('useCreateIndex', true);
+
 enum TeamState {
-	progress,
-	end,
-	less_member,
-	wait_member,
+	"progress",
+	"end",
+	"less_member",
+	"wait_member"
 };
 
 const teamSchema = new mongoose.Schema({
@@ -12,25 +14,23 @@ const teamSchema = new mongoose.Schema({
 		default: true,
 		immutable: true,
 		unique: true
-	};
-	leaderID: {type: String};
-	memberID: [{type: String}];
-	//memberID: {type: Array};
-	subject: {type: String};
-	state: {type: String};
-	//state: {
-	//	type: Number,
-	//	enum: TeamState
-	//};
+	},
+	leaderID: {type: String},
+	memberID: [{type: String}],
+	subject: {type: String},
+	state: {
+		type: String,
+		enum: TeamState
+	},
 	notionLink: {type: String
 		unique: true
-	};
+	},
 	gitLink: {type: String
 		unique: true
-	};
+	},
 	teamName: {type: String,
 		unique: true
-	};
+	}
 });
 
 const Team = mongoose.model('Team', teamSchema);
