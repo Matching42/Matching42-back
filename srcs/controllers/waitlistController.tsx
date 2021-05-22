9dd8425027e7312b25f860679346757f66a2dc3d
Lks9172 <lks9172@naver.com>
2021-05-23 01:20:54 +0900

    feat : WaitList모델update로직 구현
    - waitlist모델에서 subject_name가 일치하는 문서를 찾아 user field
      추가하는 호직 구현 완료

--- srcs/controllers/waitlistController.tsx
+++ srcs/controllers/waitlistController.tsx
@@ -2,6 +2,19 @@ import { RequestHandler } from 'express';
 import WaitList from '../models/waitlist';
 
 const addWaitList: RequestHandler = async (req, res) => {
+	try {
+		const obj_user = {user_ID: req.body.user_ID,};
+
+		await WaitList.updateOne(
+			{ subject_name: req.body.subject_name},
+			{ $push: { user: obj_user}},
+			{ new: true, upsert: true}
+		).exec();
+		res.json({result:"success"})
+	} catch (e) {
+        console.log(e.message);
+        res.json({ result: 'fail' });
+    }
 };
 
 export { addWaitList };
