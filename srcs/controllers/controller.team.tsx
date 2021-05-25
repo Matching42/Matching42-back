8b5c64373a27d70bbe49e704e75a2b70aa9a58b8
hokim <teamgoodaloe@gmail.com>
2021-05-25 16:02:45 +0900

    fix: api response 수정 - api response를 {success, data\error}으로 주도록 수정 - 에러 발생시 상태코드가 400으로 주어지도록 변경 - 파일 네임 변경

---
+++ srcs/controllers/controller.team.tsx
@@ -0,0 +1,28 @@
+import { RequestHandler } from 'express';
+import Team from '../models/TeamDB';
+
+const updateTeam: RequestHandler = async (req, res) => {
+    try {
+        const team = await Team.findOne({ ID: req.params.teamid });
+        if (req.body.state === undefined || team === null)
+            throw new Error('no such team id or state');
+        await Team.updateOne(
+            { ID: req.params.teamid },
+            { state: req.body.state },
+            { runValidators: true }
+        );
+        res.json({
+            success: true,
+        });
+    } catch (e) {
+        res.status(400).json({
+            success: false,
+            error: {
+                code: e.name,
+                message: e.message,
+            },
+        });
+    }
+};
+
+export { updateTeam };
