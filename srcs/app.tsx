bd762aef22f63dc038c99e021d043ff3219b7265
hminn <hmkim0008@gmail.com>
2021-05-26 22:35:25 +0900

    feat : body Parsing을 위한 미들웨어 세팅
    - POST 요청 시 넘어오는 데이터를 Parsing 하기 위해 필요한 미들웨어 세팅 `express.json(), express.urlencoded({ extended: false})`

--- srcs/app.tsx
+++ srcs/app.tsx
@@ -13,6 +13,8 @@ const runServer = async () => {
     const app = express();
 
     /* Set middleware */
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: false }));
     app.use(router);
 
     /* Run server */
