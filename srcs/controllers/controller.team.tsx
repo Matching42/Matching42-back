e46783951fbe391f8ff9d067b6d4bc2669c90f97
hminn <hmkim0008@gmail.com>
2021-05-27 18:10:04 +0900

    feat : addMember 컨트롤러 로직 구현
    - addMember 요청에 대한 로직 구현
    - userId에 해당하는 유저가 존재하지 않을 경우, teamId에 해당하는 팀이 존재하지 않을 경우, 팀 멤버에 이미 존재하는 유저의 요청이 들어온 경우에 대한 에러처리 구현

--- srcs/controllers/controller.team.tsx
+++ srcs/controllers/controller.team.tsx
@@ -1,7 +1,39 @@
 import { RequestHandler } from 'express';
+import Team from '../models/model.team';
+import User from '../models/model.user';
 
-const addMember: RequestHandler = (req, res) => {
-    res.json({ example: 'example' });
+const addMember: RequestHandler = async (req, res) => {
+    try {
+        const user = await User.findOne({ ID: req.body.userId });
+        const team = await Team.findOne({ ID: req.body.teamId });
+
+        if (user === null) throw new Error('Invalid User ID');
+        if (team === null) throw new Error('Invalid Team ID');
+        if (team.memberID.indexOf(user.ID) !== -1) throw new Error('The User already exists');
+        const updated_team = await Team.findOneAndUpdate(
+            { ID: team.ID },
+            { $push: { memberID: user.ID } },
+            { new: true }
+        );
+        const updated_user = await User.findOneAndUpdate(
+            { ID: user.ID },
+            { $set: { teamID: team.ID } },
+            { new: true }
+        );
+        res.status(200).json({
+            success: true,
+            team: updated_team,
+            user: updated_user,
+        });
+    } catch (error) {
+        res.status(400).json({
+            success: false,
+            error: {
+                code: error.name,
+                message: error.message,
+            },
+        });
+    }
 };
 
 export default addMember;
