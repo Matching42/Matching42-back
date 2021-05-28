6347f36e2e6f6c01b302f562c6667295decd5f23
Lks9172 <lks9172@naver.com>
2021-05-28 17:52:07 +0900

    fix : merge conflicting 이슈 해결
    - controllers/index.tsx 수정

--- srcs/controllers/index.tsx
+++ srcs/controllers/index.tsx
@@ -1,5 +1,5 @@
 export { default as exampleController } from './exampleController';
 
-// export { default as updateTeam } from './controller.team';
-// export { default as getUser } from './controller.user';
+export { default as updateTeam } from './controller.team';
+export { default as getUser } from './controller.user';
 export { default as PostWaitList } from './controller.waitlist';
