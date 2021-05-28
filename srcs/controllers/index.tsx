0452ca1eaa2cf900cbca8d1d33f1b9c774edcad2
Lks9172 <lks9172@naver.com>
2021-05-28 18:00:46 +0900

    fix : merge 마지막 해결
    - index.tsx수정

--- srcs/controllers/index.tsx
+++ srcs/controllers/index.tsx
@@ -2,4 +2,3 @@ export { default as exampleController } from './exampleController';
 
 export { default as updateTeam } from './controller.team';
 export { default as getUser } from './controller.user';
-export { default as PostWaitList } from './controller.waitlist';
