ee1d891f9e684b126f9995ee38aae12717de97ef
Lks9172 <lks9172@naver.com>
2021-05-31 15:41:48 +0900

    feat : User model관련 에러처리
    - params userId가 User모델에 존재하지 않는 경우 에러처리
    - params userId의 waitMatching값이 null일 경우 에러처리
    - 실패 시의  reponse 작성

--- srcs/controllers/controller.removeUser2WaitList.tsx
+++ srcs/controllers/controller.removeUser2WaitList.tsx
@@ -3,6 +3,12 @@ import { User, Waitlist } from '../models';
 
 const removeUser2Waitlist: RequestHandler = async (req, res) => {
     try {
+        const UserDocument = await User.findOne({ ID: req.params.userId }).exec();
+        if (UserDocument === null || UserDocument === undefined)
+            throw new Error('This user_ID does not exist.');
+        if (UserDocument.waitMatching === null)
+            throw new Error('This user_ID not registered in any subject');
+
         res.status(200).json({
             success: true,
         });
