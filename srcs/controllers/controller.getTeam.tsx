2c9e73f7821b394fd9aa44acec79224cdf231b3e
snagjaepark <atimangel@naver.com>
2021-06-02 15:14:27 +0900

    feature: teamId입력시 반환, 잘못된 값입력시 반환 -teamId를 req.params로부터 받음 -빈 값일 시 전체 팀 데이터를 반환하도록 만들어놨는데 수정할 필요 있음 -awit으로 db에서 id에대한 반환값을 기다림 값이 있을 시 정상이라는 사인 200과 함께 success: true, db에서 찾은 데이터를 반환함. -오류시 오류코드 400과 에러코드 에러메세지를 반환함

--- srcs/controllers/controller.getTeam.tsx
+++ srcs/controllers/controller.getTeam.tsx
@@ -1,1 +1,35 @@
-export default 
+import { RequestHandler } from 'express';
+import { Team } from '../models';
+
+//var limit: number = 5;
+
+const getTeam: RequestHandler = async (req, res) => {
+    try {
+        const teamId = req.params.teamId;
+        if (!teamId) {
+            //if (req.params.limit !== limit)
+            //  limit = req.params.limit;
+            const allTeams = await Team.find({});
+            res.status(200).json({
+                success: true,
+                data: allTeams
+            });
+        } else {
+            const team = await Team.findOne({ ID: teamId});
+            res.status(200).json({
+                sucess: true,
+                data: team
+            });
+        }
+    } catch (e) {
+        res.status(400).json({
+            sucess: false,
+            error: {
+                code: e.name,
+                message: e.message,
+            }
+        });
+    };
+};
+
+export default getTeam;
