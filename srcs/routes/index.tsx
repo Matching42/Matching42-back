11972018d3006db6394ef1b1114e16153b6f6c33
seo2im <seo2im6492@gmail.com>
2021-06-09 21:28:50 +0900

    feat: set swagger - use swagger-jsdoc, swagger-express-ui - set sample of swagger documentation

--- srcs/routes/index.tsx
+++ srcs/routes/index.tsx
@@ -1,11 +1,14 @@
 import { Router } from 'express';
+import swaggerUI from 'swagger-ui-express';
 import * as controller from '../controllers';
 import { fail, login, granted, isAuth } from '../auth';
+import swaggerSpec from '../swagger/option';
 
 const router = Router();
 /*
     Set your router, but must check order of router
 */
+router.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 router.get('/', (req, res) => {
     res.json({ data: 'data' });
 });
