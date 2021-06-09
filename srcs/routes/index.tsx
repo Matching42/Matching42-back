3ab822fc4e123fa543dd941b8ba46e42794c4207
seo2im <seo2im6492@gmail.com>
2021-06-09 13:23:33 +0900

    fix: set '/' for __test__

--- srcs/routes/index.tsx
+++ srcs/routes/index.tsx
@@ -6,6 +6,9 @@ const router = Router();
 /*
     Set your router, but must check order of router
 */
+router.get('/', (req, res) => {
+    res.json({ data: 'data' });
+});
 router.get('/login/42', login);
 router.get('/login/redirect', granted);
 router.get('/login/fail', fail);
@@ -16,8 +19,6 @@ router.get('/user', controller.getUser);
 router.get('/user/:userId', controller.getUser);
 router.post('/waitlist', controller.addUser2WaitList);
 router.post('/addmember', controller.addUser2Team);
-router.get('/', (req, res) => {
-    res.json({ data: 'data' });
-});
+
 
 export default router;
