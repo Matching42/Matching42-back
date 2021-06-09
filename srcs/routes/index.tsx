5046c9cd829872e11067e8e8893e49055ea7fcd3
seo2im <seo2im6492@gmail.com>
2021-06-09 13:20:40 +0900

    feat: set isAuth in route

--- srcs/routes/index.tsx
+++ srcs/routes/index.tsx
@@ -1,20 +1,18 @@
 import { Router } from 'express';
-import dotenv from 'dotenv';
 import * as controller from '../controllers';
-import { fail, login, granted } from '../auth/controller.login';
+import { fail, login, granted, isAuth } from '../auth';
 
 const router = Router();
 /*
     Set your router, but must check order of router
 */
-import { isAuth } from '../auth/auth';
-
 router.get('/login/42', login);
 router.get('/login/redirect', granted);
 router.get('/login/fail', fail);
 
+router.use(isAuth);
 router.patch('/team/:teamid', controller.updateTeamState);
-router.get('/user', isAuth, controller.getUser);
+router.get('/user', controller.getUser);
 router.get('/user/:userId', controller.getUser);
 router.post('/waitlist', controller.addUser2WaitList);
 router.post('/addmember', controller.addUser2Team);
