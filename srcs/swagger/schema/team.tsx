5246e58024b5da83eb3f1cdfb1f3a4b9fa258952
snpark <snpark@student.42seoul.kr>
2021-06-16 10:09:53 +0900

    docs: Team 스키마 swagger에 업데이트

---
+++ srcs/swagger/schema/team.tsx
@@ -0,0 +1,26 @@
+/**
+ * @swagger
+ *  components:
+ *      schemas:
+ *          Team:
+ *              type: object
+ *              properties:
+ *                  ID:
+ *                      type: string
+ *                  leaderID:
+ *                      type: string
+ *                  memberID:
+ *                      type: array
+ *                      item:
+ *                          type: string
+ *                  subject:
+ *                      type: string
+ *                  startDate:
+ *                      type: date
+ *                  notionLink:
+ *                      type: string
+ *                  gitLink
+ *                      type: string
+ *                  teamName
+ *                      type: string
+ */
