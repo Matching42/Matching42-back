b07246136d05c05b150577491b216fa4edfbd19f
hminn <hmkim0008@gmail.com>
2021-07-27 23:40:29 +0900

    fix: 오타 수정

--- srcs/lib/lib.findAllUser.tsx
+++ srcs/lib/lib.findAllUser.tsx
@@ -4,7 +4,7 @@ import { tUser } from '../../@types/types.d';
 const findAllUser: () => Promise<tUser | Error> = async () => {
     try {
         const allUsers = await User.find({});
-        if (allUsers == null) throw new Error('User not found');
+        if (allUsers === null) throw new Error('User not found');
         return allUsers;
     } catch (error) {
         return error;
--- srcs/lib/lib.findOneUser.tsx
+++ srcs/lib/lib.findOneUser.tsx
@@ -4,7 +4,7 @@ import { tUser } from '../../@types/types.d';
 const findOneUser: (userID: string) => Promise<tUser | Error> = async (userID) => {
     try {
         const user = await User.findOne({ ID: userID });
-        if (user == null) throw new Error('Invalid User ID');
+        if (user === null) throw new Error('Invalid User ID');
         return user;
     } catch (error) {
         return error;
