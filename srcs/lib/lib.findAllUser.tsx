0ddcea5a1ca6174609987653d16bc1c4cfc9ad2a
hminn <hmkim0008@gmail.com>
2021-07-28 13:26:58 +0900

    fix: 에러 처리 방식 수정
    - 회의에서 논의한 방법으로 에러 처리 진행

--- srcs/lib/lib.findAllUser.tsx
+++ srcs/lib/lib.findAllUser.tsx
@@ -1,14 +1,10 @@
 import { User } from '../models';
 import { tUser } from '../../@types/types.d';
 
-const findAllUser: () => Promise<tUser | Error> = async () => {
-    try {
-        const allUsers = await User.find({});
-        if (allUsers === null) throw new Error('User not found');
-        return allUsers;
-    } catch (error) {
-        return error;
-    }
+const findAllUser: () => Promise<tUser> = async () => {
+    const allUsers = await User.find({});
+    if (allUsers === null) throw new Error('User not found');
+    return allUsers;
 };
 
 export default findAllUser;
--- srcs/lib/lib.findOneUser.tsx
+++ srcs/lib/lib.findOneUser.tsx
@@ -1,14 +1,10 @@
 import { User } from '../models';
 import { tUser } from '../../@types/types.d';
 
-const findOneUser: (userID: string) => Promise<tUser | Error> = async (userID) => {
-    try {
-        const user = await User.findOne({ ID: userID });
-        if (user === null) throw new Error('Invalid User ID');
-        return user;
-    } catch (error) {
-        return error;
-    }
+const findOneUser: (userID: string) => Promise<tUser> = async (userID) => {
+    const user = await User.findOne({ ID: userID });
+    if (user === null) throw new Error('Invalid User ID');
+    return user;
 };
 
 export default findOneUser;
