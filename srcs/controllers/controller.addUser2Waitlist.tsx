b510ab2c81ff4b26e55956c575fdb7e70acb571d
Lks9172 <ekdan9172@gmail.com>
2021-07-29 16:22:32 +0900

    refactor: 에러처리 코드 개선
    - 랜덤 매칭 중복 대기 신청 에러처리 map메소드와 include메소드 사용하여 가독성 향상
    - 에러메시지에 문자열 리터럴 적용하여 가독성 향상
    - github api 비동기 요청 에러메시지 수정

--- srcs/controllers/controller.addUser2Waitlist.tsx
+++ srcs/controllers/controller.addUser2Waitlist.tsx
@@ -3,20 +3,23 @@ import axios from 'axios';
 import { User, Waitlist } from '../models';
 import { findOneWaitlist, findOneUser } from '../lib';
 
+const errorWaitlistCheck = (WaitlistDocument, userID, subjectName) => {
+    const checkUser = WaitlistDocument.user.map((userInfo) => userInfo.userID).includes(userID);
+    if (checkUser) {
+        throw new Error(`The user is already registered in ${subjectName} subject`);
+    }
+};
+
 const addUser2Waitlist: RequestHandler = async (req, res) => {
     try {
-        await axios.get('https://api.github.com/users/' + req.body.gitName);
+        await axios.get(`https://api.github.com/users/${req.body.gitName}`).catch((err) => {
+            throw new Error(`github API ${err.message}`);
+        });
 
         await findOneUser(req.body.userID);
 
         const WaitlistDocument = await findOneWaitlist(req.body.subjectName);
-
-        for (let i = 0; i < WaitlistDocument.user.length; i++) {
-            if (WaitlistDocument.user[i].userID === req.body.userID)
-                throw new Error(
-                    'The user is already registered in the ' + req.body.subjectName + ' subject'
-                );
-        }
+        errorWaitlistCheck(WaitlistDocument, req.body.userID, req.body.subjectName);
 
         const ChangedUser = await User.findOneAndUpdate(
             { ID: req.body.userID },
@@ -42,8 +45,6 @@ const addUser2Waitlist: RequestHandler = async (req, res) => {
             Waitlist: ChangedWaitlist,
         });
     } catch (e) {
-        if (e.request) e.message = 'not found gitName';
-
         res.status(400).json({
             success: false,
             error: {
