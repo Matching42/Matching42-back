00066260bab14bff8719778b49fa4c40dd98e282
hminn <hmkim0008@gmail.com>
2021-07-30 22:42:34 +0900

    fix: 테스트 코드 제거

--- srcs/controllers/controller.getOneUser.tsx
+++ srcs/controllers/controller.getOneUser.tsx
@@ -4,7 +4,6 @@ import { findOneUser } from '../lib';
 const getOneUser: RequestHandler = async (req, res) => {
     try {
         const user = await findOneUser(req.params.userID);
-        console.log(user);
         res.status(200).json({
             success: true,
             user: user,
