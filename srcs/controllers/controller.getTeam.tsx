457f4b1aa55727cc121bf924a07026f4e37f881d
atimangel <atimangel@naver.com>
2021-08-02 22:05:46 +0900

    docs: 라우터 파라미터 값에 대한 주석 변경

--- srcs/controllers/controller.getTeam.tsx
+++ srcs/controllers/controller.getTeam.tsx
@@ -4,7 +4,7 @@ import { findAllTeam, findOneTeam } from '../lib';
 const getTeam: RequestHandler = async (req, res) => {
     try {
         const teamID = req.params.teamID;
-        //요청이 /team으로 들어오는 경우 teamID는 undefined다.
+        //요청이 /team, /team/으로 들어오는 경우 teamID는 undefined다.
         if (teamID === undefined) {
             //parseInt는 NaN, undefinded, null이 들어올 경우 NaN을 반환하고
             //isNaN은 NaN일 경우 true를 반환한다.
@@ -34,9 +34,7 @@ const getTeam: RequestHandler = async (req, res) => {
                 success: true,
                 data: allTeams,
             });
-        }
-        //요청이 /team/으로 들어온 경우 teamID는 null이다.
-        else {
+        } else {
             const team = await findOneTeam(teamID);
             res.status(200).json({
                 sucess: true,
