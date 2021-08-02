a82c13efd7802d30c03d2961a4c304c6bade13ae
snagjaepark <atimangel@naver.com>
2021-08-02 22:48:00 +0900

    hotfix: user 키에 원소가 없을 때 [0]을 참조하려하니 filter 메소드가 에러를 냅니다 -그래서 user키가 null나 undefind 둘 다 아닌 경우로 조건을 변경했습니다. -테스트를 해보고 있습니다.

--- srcs/lib/lib.Matcher.tsx
+++ srcs/lib/lib.Matcher.tsx
@@ -27,85 +27,87 @@ const updateWaitlist = async (list, userList): Promise<void> => {
 const Matcher = async (): Promise<void> => {
     let allWaitlist = await Waitlist.find({});
     if (allWaitlist === null || allWaitlist === undefined) throw new Error('Wailist not found');
+    console.log(allWaitlist);
     //user 배열에 원소가 있는지로 필터링
     //매칭해야할 인원이 있는 서브젝트만 남음
     allWaitlist = allWaitlist.filter((list) => {
-        return list.user[0];
+        return !(list.user === null || list === undefined);
     });
-    allWaitlist.forEach((list) => {
-        //각 waitlist에 각 user별로 선호 클러스터 요청하기
-        list.user.forEach(async (user) => {
-            user.cluster = await User.findOne({ ID: user.userID });
-            if (user.cluster === null) user.cluster = '개포';
-        });
-        while (list.user.length > 0) {
-            let userID: string[];
-            let teamName: string;
-            //매칭 할 인원이 총 세명일 때
-            //매칭할 인원의 선호 클러스터가 2 : 2 일 때
-            if (
-                list.user.length === 3 ||
-                (list.user.filter((user) => user.cluster === '개포').length === 2 &&
-                    list.user.filter((user) => user.cluster === '서초').length === 2)
-            ) {
-                userID = list.user.slice(0, 3).map((user) => user.userID);
-                teamName = `${list.subject}_${userID[0]}_${Date.now()}`;
-                makeTeam(list.subject, 'progress', userID, teamName);
-                updateUser(userID, teamName);
-                updateWaitlist(list, list.user.slice(0, 3));
-                list.user.splice(0, 3);
-            }
-            // 매칭 할 인원이 1, 2명 일때
-            else if (list.user.length === 2 || list.user.length === 1) {
-                userID = list.user.map((user) => user.userID);
-                teamName = `${list.subject}_${userID[0]}_${Date.now()}`;
-                makeTeam(list.subject, 'wait_member', userID, teamName);
-                updateUser(userID, teamName);
-                updateWaitlist(list, list.user);
-                list.user = [];
-            }
-            // 선호 클러스터 개포인 인원 매칭
-            else if (list.user.filter((user) => user.cluster === '개포').length >= 3) {
-                userID = list.user
-                    .filter((user) => user.cluster === '개포')
-                    .slice(0, 3)
-                    .map((user) => user.userID);
-                teamName = `${list.subject}_${userID[0]}_${Date.now()}`;
-                makeTeam(list.subject, 'progress', userID, teamName);
-                updateUser(userID, teamName);
-                updateWaitlist(
-                    list,
-                    list.user.filter((user) => user.cluster === '개포').slice(0, 3)
-                );
-                userID.forEach((ID) =>
-                    list.splice(
-                        list.user.findIndex((user) => user.userID === ID),
-                        1
-                    )
-                );
-            }
-            // 서초 클러스터 매칭
-            else if (list.user.filter((user) => user.cluster === '서초').length >= 3) {
-                userID = list.user
-                    .filter((user) => user.cluster === '서초')
-                    .slice(0, 3)
-                    .map((user) => user.userID);
-                teamName = `${list.subject}_${userID[0]}_${Date.now()}`;
-                makeTeam(list.subject, 'progress', userID, teamName);
-                updateUser(userID, teamName);
-                updateWaitlist(
-                    list,
-                    list.user.filter((user) => user.cluster === '서초').slice(0, 3)
-                );
-                userID.forEach((ID) =>
-                    list.splice(
-                        list.user.findIndex((user) => user.userID === ID),
-                        1
-                    )
-                );
-            }
-        }
-    });
+    console.log(allWaitlist);
+    // allWaitlist.forEach((list) => {
+    //     //각 waitlist에 각 user별로 선호 클러스터 요청하기
+    //     list.user.forEach(async (user) => {
+    //         user.cluster = await User.findOne({ ID: user.userID });
+    //         if (user.cluster === null) user.cluster = '개포';
+    //     });
+    //     while (list.user.length > 0) {
+    //         let userID: string[];
+    //         let teamName: string;
+    //         //매칭 할 인원이 총 세명일 때
+    //         //매칭할 인원의 선호 클러스터가 2 : 2 일 때
+    //         if (
+    //             list.user.length === 3 ||
+    //             (list.user.filter((user) => user.cluster === '개포').length === 2 &&
+    //                 list.user.filter((user) => user.cluster === '서초').length === 2)
+    //         ) {
+    //             userID = list.user.slice(0, 3).map((user) => user.userID);
+    //             teamName = `${list.subject}_${userID[0]}_${Date.now()}`;
+    //             makeTeam(list.subject, 'progress', userID, teamName);
+    //             updateUser(userID, teamName);
+    //             updateWaitlist(list, list.user.slice(0, 3));
+    //             list.user.splice(0, 3);
+    //         }
+    //         // 매칭 할 인원이 1, 2명 일때
+    //         else if (list.user.length === 2 || list.user.length === 1) {
+    //             userID = list.user.map((user) => user.userID);
+    //             teamName = `${list.subject}_${userID[0]}_${Date.now()}`;
+    //             makeTeam(list.subject, 'wait_member', userID, teamName);
+    //             updateUser(userID, teamName);
+    //             updateWaitlist(list, list.user);
+    //             list.user = [];
+    //         }
+    //         // 선호 클러스터 개포인 인원 매칭
+    //         else if (list.user.filter((user) => user.cluster === '개포').length >= 3) {
+    //             userID = list.user
+    //                 .filter((user) => user.cluster === '개포')
+    //                 .slice(0, 3)
+    //                 .map((user) => user.userID);
+    //             teamName = `${list.subject}_${userID[0]}_${Date.now()}`;
+    //             makeTeam(list.subject, 'progress', userID, teamName);
+    //             updateUser(userID, teamName);
+    //             updateWaitlist(
+    //                 list,
+    //                 list.user.filter((user) => user.cluster === '개포').slice(0, 3)
+    //             );
+    //             userID.forEach((ID) =>
+    //                 list.splice(
+    //                     list.user.findIndex((user) => user.userID === ID),
+    //                     1
+    //                 )
+    //             );
+    //         }
+    //         // 서초 클러스터 매칭
+    //         else if (list.user.filter((user) => user.cluster === '서초').length >= 3) {
+    //             userID = list.user
+    //                 .filter((user) => user.cluster === '서초')
+    //                 .slice(0, 3)
+    //                 .map((user) => user.userID);
+    //             teamName = `${list.subject}_${userID[0]}_${Date.now()}`;
+    //             makeTeam(list.subject, 'progress', userID, teamName);
+    //             updateUser(userID, teamName);
+    //             updateWaitlist(
+    //                 list,
+    //                 list.user.filter((user) => user.cluster === '서초').slice(0, 3)
+    //             );
+    //             userID.forEach((ID) =>
+    //                 list.splice(
+    //                     list.user.findIndex((user) => user.userID === ID),
+    //                     1
+    //                 )
+    //             );
+    //         }
+    //     }
+    // });
 };
 
 export default Matcher;
