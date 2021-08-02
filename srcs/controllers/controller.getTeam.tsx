98f568c7e69a0fa456fc03312e72b60004eee0d5
atimangel <atimangel@naver.com>
2021-08-02 09:36:33 +0900

    refactor: 조건문 변경 -teamID가 null일 경우 findOneTeam으로 간다 -page, limit쿼리 값이 모두 undefined가 아니라면 페이지네이션을 한다. -parseInt 반환 값이 NaN일 경우도 있기 때문에 isNaN을 써서 undefine, null, NaN을 모두 처리해준다.

--- srcs/controllers/controller.getTeam.tsx
+++ srcs/controllers/controller.getTeam.tsx
@@ -1,19 +1,19 @@
 import { RequestHandler } from 'express';
-import { Team } from '../models';
 import { findAllTeam, findOneTeam } from '../lib';
 
 const getTeam: RequestHandler = async (req, res) => {
     try {
         const teamID = req.params.teamID;
-        if (!teamID) {
-            const limit =
-                req.query.limit === undefined || req.query.limit === null
-                    ? 5
-                    : parseInt(req.query.limit as string);
-            const page =
-                req.query.page === undefined || req.query.page === null
-                    ? 0
-                    : parseInt(req.query.page as string);
+        //요청이 /team으로 들어오는 경우 teamID는 undefined다.
+        if (teamID === undefined) {
+            //parseInt는 NaN, undefinded, null이 들어올 경우 NaN을 반환하고
+            //isNaN은 NaN일 경우 true를 반환한다.
+            const limit = isNaN(parseInt(req.query.limit as string))
+                ? 5
+                : parseInt(req.query.limit as string);
+            const page = isNaN(parseInt(req.query.page as string))
+                ? 0
+                : parseInt(req.query.page as string);
             let allTeams = await findAllTeam();
             if ((req.query.progress as string) === 'true') {
                 allTeams = allTeams.filter((team) => {
@@ -25,19 +25,17 @@ const getTeam: RequestHandler = async (req, res) => {
                     return team.subject === req.query.subject;
                 });
             }
-            if (
-                req.query.page !== undefined ||
-                req.query.limit !== undefined ||
-                req.query.page !== null ||
-                req.query.limit !== null
-            ) {
+            //페이지네이션을 하겠다는 신호를 page, limit 쿼리중 하나라도 요청에 포함해서 보냈느냐로 판단한다.
+            if (req.query.limit !== undefined || req.query.page !== undefined) {
                 allTeams = allTeams.slice(limit * page, limit * page + limit);
             }
             res.status(200).json({
                 success: true,
                 data: allTeams,
             });
-        } else {
+        }
+        //요청이 /team/으로 들어온 경우 teamID는 null이다.
+        else {
             const team = await findOneTeam(teamID);
             res.status(200).json({
                 sucess: true,
