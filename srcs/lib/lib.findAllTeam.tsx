88b5553907aae2f1ba3cb0c89f8a495722bdae49
hokim <teamgoodaloe@gmail.com>
2021-08-02 10:13:34 +0900

    fix: findAllTeam 리턴 타입 변경 -리턴타입 배열로 변경

--- srcs/lib/lib.findAllTeam.tsx
+++ srcs/lib/lib.findAllTeam.tsx
@@ -1,7 +1,7 @@
 import { Team } from '../models';
 import { tTeam } from '../../@types/types.d';
 
-const findAllTeam: () => Promise<tTeam> = async () => {
+const findAllTeam: () => Promise<tTeam[]> = async () => {
     const allTeam = await Team.find({});
     if (allTeam === null || allTeam === undefined) throw new Error('Team not found');
     return allTeam;
