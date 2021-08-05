6c7ba97f39c245649b1461b1f66f10538075a52d
snagjaepark <atimangel@naver.com>
2021-08-05 18:02:25 +0900

    refactor: Matcher error 핸들링 -서버에 요청한 이후 받는 promise가 거부 될 경우 rejection 메소드로 에러 throw -error가 발생한 경우 catch해서 console.error()로 출력 -user의 teamID가 있는 경우 teamID로 team정보 조회해서 state가 end가 아닐 경우 에러 발생

--- srcs/lib/lib.Matcher.tsx
+++ srcs/lib/lib.Matcher.tsx
@@ -1,6 +1,7 @@
 import { Team } from '../models';
 import { Waitlist } from '../models';
 import { User } from '../models';
+import { findOneUser, findOneTeam } from '../lib';
 
 const makeTeam = async (subject: string, state: string, user, teamID): Promise<void> => {
     const team = new Team({
@@ -14,12 +15,14 @@ const makeTeam = async (subject: string, state: string, user, teamID): Promise<v
         gitLink: null,
         teamName: teamID,
     });
-    await team.save();
+    await team.save().reject(new Error(`Error: save new team on DB fail`));
 };
 
 const updateUser = (userID: string[], teamName: string) => {
     userID.forEach(async (Id) => {
-        await User.updateOne({ ID: Id }, { $set: { waitMatching: null, teamID: teamName } });
+        await User.updateOne({ ID: Id }, { $set: { waitMatching: null, teamID: teamName } }).reject(
+            new Error(`Error: update user information on DB fail`)
+        );
     });
 };
 
@@ -27,7 +30,10 @@ const updateWaitlist = async (list, userList): Promise<void> => {
     userList.forEach(async (user) => {
         user.userInfo = undefined;
         user.cluster = undefined;
-        await Waitlist.updateOne({ subjectName: list.subjectName }, { $pull: { user: user } });
+        await Waitlist.updateOne(
+            { subjectName: list.subjectName },
+            { $pull: { user: user } }
+        ).reject(new Error(`Error: update waitlist on DB fail`));
     });
 };
 
@@ -36,71 +42,91 @@ const genTeamName = (subject: string, user: string): string => {
 };
 
 const getUserInfo = async (user): Promise<void> => {
-    user.userInfo = await User.findOne({ ID: user.userID });
-    user.cluster = user.userInfo.cluster;
-    if (user.cluster === null) user.cluster = '개포';
+    try {
+        user.userInfo = findOneUser(user);
+        user.cluster = user.userInfo.cluster;
+        if (user.cluster === null) user.cluster = '개포';
+        //매칭을 신청한 유저가 종료되지 않은 팀에 속해 있다면 에러를 출력
+        if (user.teamID) {
+            user.userInfo.team = findOneTeam(user.userInfo.teamID);
+            if (user.userInfo.team.state !== 'end')
+                throw new Error(`Error: ${user.ID} is already in ${user.userInfo.teamID}`);
+        }
+    } catch (error) {
+        console.error(error);
+    }
 };
 
 const matching = async (list): Promise<void> => {
-    while (list.user.length > 0) {
-        let userID: string[];
-        let teamName: string;
-        // 매칭 할 인원이 총 세명일 때
-        // 매칭할 인원의 선호 클러스터가 2 : 2 일 때
-        if (
-            list.user.length === 3 ||
-            (list.user.filter((user) => user.cluster === '개포').length === 2 &&
-                list.user.filter((user) => user.cluster === '서초').length === 2)
-        ) {
-            userID = list.user.slice(0, 3).map((user) => user.userID);
-            teamName = genTeamName(list.subjectName, userID[0]);
-            makeTeam(list.subjectName, 'progress', userID, teamName);
-            updateUser(userID, teamName);
-            updateWaitlist(list, list.user.slice(0, 3));
-            list.user.splice(0, 3);
-        }
-        //매칭 할 인원이 1, 2명 일때
-        else if (list.user.length === 2 || list.user.length === 1) {
-            userID = list.user.map((user) => user.userID);
-            teamName = genTeamName(list.subjectName, userID[0]);
-            makeTeam(list.subjectName, 'wait_member', userID, teamName);
-            updateUser(userID, teamName);
-            updateWaitlist(list, list.user);
-            list.user = [];
-        }
-        // 선호 클러스터 개포인 인원 매칭
-        else if (list.user.filter((user) => user.cluster === '개포').length >= 3) {
-            userID = list.user
-                .filter((user) => user.cluster === '개포')
-                .slice(0, 3)
-                .map((user) => user.userID);
-            teamName = genTeamName(list.subjectName, userID[0]);
-            makeTeam(list.subjectName, 'progress', userID, teamName);
-            updateUser(userID, teamName);
-            updateWaitlist(list, list.user.filter((user) => user.cluster === '개포').slice(0, 3));
-            for (const id of userID) {
-                list.user = list.user.filter((user) => user.userID !== id);
+    try {
+        while (list.user.length > 0) {
+            let userID: string[];
+            let teamName: string;
+            // 매칭 할 인원이 총 세명일 때
+            // 매칭할 인원의 선호 클러스터가 2 : 2 일 때
+            if (
+                list.user.length === 3 ||
+                (list.user.filter((user) => user.cluster === '개포').length === 2 &&
+                    list.user.filter((user) => user.cluster === '서초').length === 2)
+            ) {
+                userID = list.user.slice(0, 3).map((user) => user.userID);
+                teamName = genTeamName(list.subjectName, userID[0]);
+                makeTeam(list.subjectName, 'progress', userID, teamName);
+                updateUser(userID, teamName);
+                updateWaitlist(list, list.user.slice(0, 3));
+                list.user.splice(0, 3);
+            }
+            //매칭 할 인원이 1, 2명 일때
+            else if (list.user.length === 2 || list.user.length === 1) {
+                userID = list.user.map((user) => user.userID);
+                teamName = genTeamName(list.subjectName, userID[0]);
+                makeTeam(list.subjectName, 'wait_member', userID, teamName);
+                updateUser(userID, teamName);
+                updateWaitlist(list, list.user);
+                list.user = [];
+            }
+            // 선호 클러스터 개포인 인원 매칭
+            else if (list.user.filter((user) => user.cluster === '개포').length >= 3) {
+                userID = list.user
+                    .filter((user) => user.cluster === '개포')
+                    .slice(0, 3)
+                    .map((user) => user.userID);
+                teamName = genTeamName(list.subjectName, userID[0]);
+                makeTeam(list.subjectName, 'progress', userID, teamName);
+                updateUser(userID, teamName);
+                updateWaitlist(
+                    list,
+                    list.user.filter((user) => user.cluster === '개포').slice(0, 3)
+                );
+                for (const id of userID) {
+                    list.user = list.user.filter((user) => user.userID !== id);
+                }
+            }
+            // 서초 클러스터 매칭
+            else if (list.user.filter((user) => user.cluster === '서초').length >= 3) {
+                userID = list.user
+                    .filter((user) => user.cluster === '서초')
+                    .slice(0, 3)
+                    .map((user) => user.userID);
+                teamName = genTeamName(list.subjectName, userID[0]);
+                makeTeam(list.subjectName, 'progress', userID, teamName);
+                updateUser(userID, teamName);
+                updateWaitlist(
+                    list,
+                    list.user.filter((user) => user.cluster === '서초').slice(0, 3)
+                );
+                for (const id of userID) {
+                    list.user = list.user.filter((user) => user.userID !== id);
+                }
             }
         }
-        // 서초 클러스터 매칭
-        else if (list.user.filter((user) => user.cluster === '서초').length >= 3) {
-            userID = list.user
-                .filter((user) => user.cluster === '서초')
-                .slice(0, 3)
-                .map((user) => user.userID);
-            teamName = genTeamName(list.subjectName, userID[0]);
-            makeTeam(list.subjectName, 'progress', userID, teamName);
-            updateUser(userID, teamName);
-            updateWaitlist(list, list.user.filter((user) => user.cluster === '서초').slice(0, 3));
-            for (const id of userID) {
-                list.user = list.user.filter((user) => user.userID !== id);
-            }
-        }
+    } catch (error) {
+        console.error(error);
     }
 };
 
 const Matcher = async (): Promise<void> => {
-    let allWaitlist = await Waitlist.find({});
+    let allWaitlist = await Waitlist.find({}).reject(new Error(`Error: find Waitlist on DB fail`));
     if (allWaitlist === null || allWaitlist === undefined) throw new Error('Wailist not found');
     //user 배열에 원소가 있는지로 필터링
     //매칭해야할 인원이 있는 서브젝트만 남음
@@ -110,11 +136,11 @@ const Matcher = async (): Promise<void> => {
     //각 waitlist에 각 user별로 선호 클러스터 요청하기
     for (const list of allWaitlist) {
         for (const user of list.user) {
-            await getUserInfo(user);
+            await getUserInfo(user).catch((error) => console.error(error));
         }
     }
     for (const list of allWaitlist) {
-        await matching(list);
+        await matching(list).catch((error) => console.error(error));
     }
 };
 
