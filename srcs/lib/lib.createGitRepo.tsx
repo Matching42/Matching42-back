22a0ab2b8941cd71fcb3d61faaf513c8830d7134
hokim <teamgoodaloe@gmail.com>
2021-08-17 21:00:14 +0900

    fix: 매개변수 수정 - 매개변수 수정 - 에러를 로거로 찍도록 수정

--- srcs/lib/lib.createGitRepo.tsx
+++ srcs/lib/lib.createGitRepo.tsx
@@ -1,22 +1,14 @@
 import axios from 'axios';
 import dotenv from 'dotenv';
-import { findOneTeam } from '../lib';
+import { logger } from '../config/winston';
 
 dotenv.config();
 
-const checkError = (team) => {
-    if (team.subject === undefined)
-        throw new Error('\x1b[31mError: lib.createGitRepo: Team has NO subject\x1b[0m\n');
-    if (team.gitLink)
-        throw new Error('\x1b[31mError: lib.createGitRepo: Team already has gitLink\x1b[0m\n');
-    if (team.state === 'end')
-        throw new Error('\x1b[31mError: lib.createGitRepo: Team state is "end"\x1b[0m\n');
-};
-
-const createGitRepo: (teamID: string) => Promise<string | null> = async (teamID) => {
+const createGitRepo: (subject: string, ID: string) => Promise<string | null> = async (
+    subject,
+    ID
+) => {
     try {
-        const team = await findOneTeam(teamID);
-        checkError(team);
         const createResult = await axios({
             method: 'post',
             url: `https://api.github.com/orgs/${process.env.ORG_NAME}/repos`,
@@ -25,14 +17,14 @@ const createGitRepo: (teamID: string) => Promise<string | null> = async (teamID)
                 Authorization: `token ${process.env.GIT_TOKEN}`,
             },
             data: {
-                name: `Matching42_${team.subject}_${team.ID}`,
+                name: `Matching42_${subject}_${ID}`,
                 auto_init: true,
                 private: true,
             },
         });
         return createResult.data.html_url;
     } catch (e) {
-        console.error(e.message);
+        logger.error(e.message);
         return null;
     }
 };
