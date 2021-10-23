24aaaf7ddb3f383ae464eb48fdfa81e560326858
Lks9172 <ekdan9172@naver.com>
2021-10-23 20:42:02 +0900

    feat: addUser2Team api문서 수정
    - addUser2Team api 수정사항 api문서에도 반영

--- srcs/swagger/controller/addUser2Team.tsx
+++ srcs/swagger/controller/addUser2Team.tsx
@@ -9,6 +9,7 @@
  *              #### 유저를 등록시킬 팀의 ID와 팀에 추가할 유저의 ID가 필요합니다.
  *              - teamID - 유저를 추가하고자 하는 팀 ID
  *              - userID - 팀에 추가하고자 하는 유저의 ID
+ *              - gitName - 유저의 github ID
  *          content:
  *              application/json:
  *                  schema:
@@ -20,9 +21,13 @@
  *                          userID:
  *                              type: string
  *                              required: true
+ *                          gitName:
+ *                              type: string
+ *                              required: true
  *                      example:
  *                          "teamID": "1"
  *                          "userID": "hyeokim"
+ *                          "gitName": "hminn"
  *      responses:
  *          200:
  *              description: |
@@ -49,7 +54,7 @@
  *                          - level : 유저의 42SEOUL 레벨 정보 </br>
  *                      - waitMatching : 유저의 매칭 대기 상태 정보 </br>
  *                      - teamID : 유저가 속해 있는 팀의 ID </br>
- *                      - gitID : 유저의 Github ID </br>
+ *                      - gitName : 유저의 Github Username </br>
  *                      - cluster : 유저가 선호하는 클러스터
  *              content:
  *                  application/json:
@@ -67,6 +72,7 @@
  *                  잘못된 요청으로 인해 멤버 추가 요청 실패 </br>
  *                  - 유효하지 않은 teamID 값이 들어온 경우 </br>
  *                  - 유효하지 않은 userID 값이 들어온 경우</br>
+ *                  - 유효하지 않은 gitName 값이 들어온 경우 </br>
  *                  - 팀 멤버 리스트에 이미 해당 유저가 존재하는 경우 </br>
  *                  - 팀의 상태가 "wait_member"가 아닌 경우
  *              content:
